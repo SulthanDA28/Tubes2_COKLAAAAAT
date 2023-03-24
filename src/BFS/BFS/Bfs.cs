4de51351bb484365a319c754c68db53fca28ece3
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2023-03-24 15:58:42 +0700

    BFS

---
+++ src/BFS/BFS/Bfs.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.IO;
+using System.Runtime.ExceptionServices;
+
+class Bfs
+{
+    static public List<Node> bfs(Graph g, int treasureCount)    // Algoritma BFS utama
+    {
+        Node n;
+        Node end;
+        Node startNode = g.getStartNode();
+        List<Node> treasures = new List<Node>();
+        List<Node> neighbors;
+        Queue<Node> queue = new Queue<Node>();
+
+        queue.Enqueue(startNode);
+        startNode.setMarked(true);
+
+        while (queue.Count > 0 && treasureCount > 0)
+        {
+            n = queue.Dequeue();
+            neighbors = g.getNeighbors(n);
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                if (!neighbors[i].getMarked())
+                {
+                    if (neighbors[i].getNodeType() == 'T')
+                    {
+                        
+                        end = neighbors[i];
+                        treasures.Add(end);
+                        treasureCount--;
+                    }
+                    queue.Enqueue(neighbors[i]);
+                    neighbors[i].setMarked(true);
+                    neighbors[i].setPrevNode(n);
+                }
+            }
+        }
+        return treasures;   // Mengembalikan semua treasure yang ditemukan untuk dibangun path/rutenya
+    }
+
+    static public List<List<Node>> constructPath(List<Node> treasures)  // Untuk membangun path/rute dari treasure ke starting node, yaitu 'K'
+    {
+        
+        List<List<Node>> paths = new List<List<Node>>();
+
+        for (int i = 0; i < treasures.Count; i++)
+        {
+            Node current = treasures[i];
+            paths.Add(new List<Node>());
+            while (current.getPrevNode() != null)
+            {
+                paths[i].Add(current);
+                current = current.getPrevNode();
+            }
+            paths[i].Add(current);
+
+            paths[i].Reverse();
+        }
+
+        return paths;
+    }
+
+    static public void readFileForBFS(Graph g, string textFile) // Membaca file input dan mengubahnya ke graf, dibuat khusus untuk BFS karena perbedaan representasi graf
+    {
+        List<Node> nodes = new List<Node>();
+
+        string text = File.ReadAllText(textFile);
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '\n')
+            {
+                y--;
+                x = 0;
+            }
+            if (text[i] == 'T' || text[i] == 'R' || text[i] == 'K')
+            {
+                nodes.Add(new Node(x, y, text[i]));
+            }
+            if (text[i] == 'T' || text[i] == 'R' || text[i] == 'K' || text[i] == 'X')
+            {
+                x--;
+            }
+        }
+
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            g.addNode(nodes[i], new List<Node> { });
+            for (int j = 0; j < nodes.Count; j++)
+            {
+                if (i != j)
+                {
+                    if (nodes[j].getX() == nodes[i].getX() || nodes[j].getX() == nodes[i].getX())
+                    {
+                        if (nodes[j].getY() == nodes[i].getY() + 1 || nodes[j].getY() == nodes[i].getY() - 1)
+                        {
+                            g.addNeighbor(nodes[i], nodes[j]);
+                        }
+                    }
+                    if (nodes[j].getY() == nodes[i].getY() || nodes[j].getY() == nodes[i].getY())
+                    {
+                        if (nodes[j].getX() == nodes[i].getX() + 1 || nodes[j].getX() == nodes[i].getX() - 1)
+                        {
+                            g.addNeighbor(nodes[i], nodes[j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    static public void printAllPath(List<List<Node>> paths)
+    {
+        for (int i = 0; i < paths.Count; i++)
+        {
+            Console.WriteLine("Path " + (i+1) + " :");
+            for (int j = 0; j < paths[i].Count; j++)
+            {
+                Console.WriteLine("(" + paths[i][j].getX() + ", " + paths[i][j].getY() + ")");
+            }
+
+        }
+    }
+
+    static public void Main(String[] args)
+    {
+        List<List<Node>> paths;
+        int treasureCount;
+
+        // Testcase dari Spek
+        Console.WriteLine("Testcase dari Spek");
+
+        Graph g1 = new Graph();
+        readFileForBFS(g1, "../../../tc.txt");
+        treasureCount = g1.getTreasureCount();
+        paths = constructPath(bfs(g1, treasureCount));
+        printAllPath(paths);
+
+        // Sampel-1
+        Console.WriteLine("\nSampel-1");
+
+        Graph g2 = new Graph();
+        readFileForBFS(g2, "../../../sampel-1.txt");
+        treasureCount = g2.getTreasureCount();
+        paths = constructPath(bfs(g2, treasureCount));
+        printAllPath(paths);
+
+        // Sampel-2
+        Console.WriteLine("\nSampel-2");
+
+        Graph g3 = new Graph();
+        readFileForBFS(g3, "../../../sampel-2.txt");
+        treasureCount = g3.getTreasureCount();
+        paths = constructPath(bfs(g3, treasureCount));
+        printAllPath(paths);
+
+        // Sampel-4
+        Console.WriteLine("\nSampel-4");
+
+        Graph g4 = new Graph();
+        readFileForBFS(g4, "../../../sampel-4.txt");
+        treasureCount = g4.getTreasureCount();
+        paths = constructPath(bfs(g4, treasureCount));
+        printAllPath(paths);
+
+        // Sampel-5
+        Console.WriteLine("\nSampel-5");
+
+        Graph g5 = new Graph();
+        readFileForBFS(g5, "../../../sampel-5.txt");
+        treasureCount = g5.getTreasureCount();
+        paths = constructPath(bfs(g5, treasureCount));
+        printAllPath(paths);
+    }
+}
---
+++ src/BFS/BFS/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Node
+{
+    private int x;
+    private int y;
+    private char nodeType;
+    private bool marked;
+    private Node prevNode;
+
+    public Node(int x, int y, char nodeType)
+    {
+        this.x = x;
+        this.y = y;
+        this.nodeType = nodeType;
+        this.marked = false;
+        this.prevNode = null;
+    }
+
+    public int getX() { return this.x;}
+    public int getY() { return this.y;}
+    public char getNodeType() { return this.nodeType;}
+    public bool getMarked() { return this.marked;}
+    public void setMarked(bool marked) { this.marked = marked;}
+    public Node getPrevNode() { return this.prevNode;}
+    public void setPrevNode(Node prevNode) { this.prevNode = prevNode; }
+}
